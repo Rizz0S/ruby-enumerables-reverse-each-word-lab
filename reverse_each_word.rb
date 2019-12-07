59c28dbda275ea72fe73ff22cbd817ab0a7797d8
Summer Rizzo <srainrizzo@gmail.com>
2019-12-07 23:06:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(source)
   source_arr = source.split(",")
   puts source_arr
   new = []
-  source_arr.each do |reversed|
-    new << source_arr.reverse
+  source_arr.each do |str|
+    new << str.reverse
   end
   new
 end
